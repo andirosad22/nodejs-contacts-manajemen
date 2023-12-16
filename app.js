a5ccf1051030825287e3fecafb683584b926d3b3
andirosad22 <andirosad22@gmail.com>
2023-12-16 09:04:04 +0700

    Mengambil argument dari command lin with process.argv

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const contacts = require('./contacts');
-const main = async () => {
-  const nama = await contacts.tulisPertanyaan('Masukan Nama: ');
-  const email = await contacts.tulisPertanyaan('Masukan Email: ');
-  const noHp = await contacts.tulisPertanyaan('Masukan No HP: ');
-  
-  contacts.simpanContact(nama, email, noHp);
-}
+const { log } = require("console");
+const yargs = require('yargs');
 
-main();
+// Mengambil argument dari command line
+// console.log(process.argv[2]);
+
+console.log(yargs.argv);
+
+
+
+
+
+
+
+// const contacts = require('./contacts');
+// const main = async () => {
+//   const nama = await contacts.tulisPertanyaan('Masukan Nama: ');
+//   const email = await contacts.tulisPertanyaan('Masukan Email: ');
+//   const noHp = await contacts.tulisPertanyaan('Masukan No HP: ');
+  
+//   contacts.simpanContact(nama, email, noHp);
+// }
+
+// main();
